ecdb334352cd28d2b031be2bb9828c58b0e46113
selectjundev <selectjun.dev@gmail.com>
2023-09-16 18:56:43 +0900

    [ feat ] 프로젝트 초기화

---
+++ src/test/java/com/attendancecheck/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendancecheck.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
